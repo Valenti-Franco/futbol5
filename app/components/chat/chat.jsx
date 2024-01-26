8e88a80b708ad7fbba3c73a0007c41a6b1f88e82
Valenti-Franco <valentifranco23@gmail.com>
2024-01-26 07:15:21 -0300

    Futbol 5

--- app/components/chat/chat.jsx
+++ app/components/chat/chat.jsx
@@ -301,6 +301,8 @@ const Chat = () => {
                   class="flex items-center justify-center w-full space-x-2"
                 >
                   <input
+                    onFocus={() => setIsChat(true)}
+                    // onBlur={() => setIsChat(true)}
                     name="message"
                     onChange={(e) => setNewMessage(e.target.value)}
                     class="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50  focus-visible:ring-offset-2"
